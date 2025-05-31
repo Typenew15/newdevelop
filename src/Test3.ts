42f4dd571eae9cda096a98c78f92aab5e06c7328
Typenew15 <balajishanmugamm@gmail.com>
2025-05-31 22:25:07 +0530

    Update Test3.ts
    commit from github

--- src/Test3.ts
+++ src/Test3.ts
@@ -1,11 +1,11 @@
-// let fruits = ['banana', 'apple', 'grapes', 'orange'];
-// let result = fruits.sort();
-// console.log(result)
+let fruits = ['banana', 'apple', 'grapes', 'orange'];
+let result = fruits.sort();
+console.log(result)
 
-// let nums = [1, 3, 7, 10, 12, 15];
-// let result = nums.find(num => num % 2 === 0)
-// console.log(result)
+let nums = [1, 3, 7, 10, 12, 15];
+let result = nums.find(num => num % 2 === 0)
+console.log(result)
 
-// let fruits = ['banana', 'apple', 'grapes', 'apple', 'orange'];
-// let result = fruits.indexOf('apple')
-// console.log(result)
+let fruits = ['banana', 'apple', 'grapes', 'apple', 'orange'];
+let result = fruits.indexOf('apple')
+console.log(result)

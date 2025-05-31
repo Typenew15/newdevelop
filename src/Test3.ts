d3ccb004d7900fd1764d6f4d8fb32e0c2bea1a05
Pygrad <balajishank@gmail.com>
2025-05-31 22:32:40 +0530

    Added cooment out

--- src/Test3.ts
+++ src/Test3.ts
@@ -7,5 +7,5 @@
 // console.log(result)
 
 // let fruits = ['banana', 'apple', 'grapes', 'apple', 'orange'];
-// let result = fruits.indexOf('apple')
+// let result = fruits.indexOf('apple','orange)//cooment out
 // console.log(result)

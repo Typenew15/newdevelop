f60022ccbe03a9c9ebf6a35e7189e2515030d8b2
Pygrad <balajishank@gmail.com>
2025-05-31 22:10:53 +0530

    Adding Testing2

---
+++ src/Test2.ts
@@ -0,0 +1,12 @@
+// let arr = [1,11,50,17,2,3,4,5,10]
+
+// let result = arr.filter(num => num >10);
+// console.log(result)
+
+// let arr1 = [1,2,3,4,5]
+// let result1 = arr1.map(num => num * num)
+// console.log(result1)
+
+// let arr = [1,2,3,4,5]
+// let result = arr.reduce((acc, curr)=>acc+curr,0)
+// console.log(result)

30d55902c1697d6d61cd44a46cb5a244b1fa8018
Pygrad <balajishank@gmail.com>
2025-06-07 19:05:37 +0530

    updates Test2.ts

--- src/Test2.ts
+++ src/Test2.ts
@@ -3,10 +3,10 @@
 // let result = arr.filter(num => num >10);
 // console.log(result)
 
-// let arr1 = [1,2,3,4,5]
-// let result1 = arr1.map(num => num * num)
-// console.log(result1)
+let arr1 = [1,2,3,4,5]
+let result1 = arr1.map(num => num * num)
+console.log(result1)
 
-// let arr = [1,2,3,4,5]
-// let result = arr.reduce((acc, curr)=>acc+curr,0)
-// console.log(result)
+let arr = [1,2,3,4,5]
+let result = arr.reduce((acc, curr)=>acc+curr,0)
+console.log(result)

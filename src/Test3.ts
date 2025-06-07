dacab47b739239e1ef3a7c3b92f09221d53a71c4
Pygrad <balajishank@gmail.com>
2025-06-07 18:56:44 +0530

    Updated Test3.ts

--- src/Test3.ts
+++ src/Test3.ts
@@ -6,6 +6,23 @@ let nums = [1, 3, 7, 10, 12, 15];
 let result = nums.find(num => num % 2 === 0)
 console.log(result)
 
+console.log("Test the conflicts")
+
 // let fruits = ['banana', 'apple', 'grapes', 'apple', 'orange'];
 // let result = fruits.indexOf('apple','orange)//cooment out new try
 // console.log(result)
+
+test.beforeEach(async ({page}) => {
+    await page.addInitScript(() => {
+      class FileSystemFileHandleMock {
+        constructor(file) {
+          this._file = file;
+        }
+  
+        async getFile() {
+          return this._file;
+        }
+      }
+      window.showOpenFilePicker = async () => [new FileSystemFileHandleMock(new File(['Test content.'], "foo.txt"))];
+    });
+  });

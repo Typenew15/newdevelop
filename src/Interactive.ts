fba40edb07701ab541fb1b0bc86e2001ad90b6b0
Typenew15 <balajishanmugamm@gmail.com>
2025-06-17 12:19:11 +0530

    Update Interactive.ts

--- src/Interactive.ts
+++ src/Interactive.ts
@@ -4,3 +4,21 @@ function sum1(...numbers: number[]): number {
   
   console.log(sum1(1, 2, 3));
   console.log(sum1(10, 20));
+
+abstract class Animal1 {
+  abstract makeSound(): void;
+
+  move(): void {
+    console.log('Moving...');
+  }
+}
+
+class Dog1 extends Animal1 {
+  makeSound(): void {
+    console.log('Bark');
+  }
+}
+
+const myDog = new Dog1();
+myDog.makeSound();
+myDog.move();

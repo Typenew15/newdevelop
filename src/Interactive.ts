ed91d6310410cea499bf096457876ec3a4e4c670
Pygrad <balajishank@gmail.com>
2025-06-17 12:21:00 +0530

    Added Interface

--- src/Interactive.ts
+++ src/Interactive.ts
@@ -4,3 +4,24 @@ function sum1(...numbers: number[]): number {
   
   console.log(sum1(1, 2, 3));
   console.log(sum1(10, 20));
+
+  interface Shape {
+    calculateArea(): number;
+  }
+  
+  class Rectangle implements Shape {
+    width: number;
+    height: number;
+  
+    constructor(width: number, height: number) {
+      this.width = width;
+      this.height = height;
+    }
+  
+    calculateArea(): number {
+      return this.width * this.height;
+    }
+  }
+  
+  const rect = new Rectangle(5, 10);
+  console.log(rect.calculateArea());
